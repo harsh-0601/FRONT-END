d2b347363ee615897ebf4c3d40008f61b39d4aeb
harsh-0601 <harsh.tiwari603@gmail.com>
2022-06-09 22:25:22 +0530

    commit

--- listApp.js
+++ listApp.js
@@ -9,6 +9,7 @@ taskFormEl.addEventListener("submit",function(){
     //Set to Local Storage 
      localStorage.setItem("tasks", JSON.stringify(taskList));
      displayTask();
+     window.reload();
 });
 
 //to display Task
@@ -18,14 +19,33 @@ let displayTask= () => {
     if(taskList.length !== 0){
         let eachTask = "";
         for(let task of taskList){
-            eachTask +=` <li class="list-group-items list-group-item-action list-group-item-warning">
-            <span class="font-weight-bold">${task}</span>
-            <button class="close btn-outline-primary">
-                <i class="fa fa-times-circle m-1" value="close"></i>
-            </button>
+            eachTask +=`<li class="list-group-items list-group-item-action list-group-item-warning p-2">
+            <span class="font-weight-bold">${task}
+            <i class="fa fa-times-circle m-1 fa-lg" value="close"></i>
+            </span>
             </li>`
         };
         taskListEl.innerHTML = eachTask;
     }
 };
 displayTask();
+
+//To Delete A task
+let taskListEl = document.querySelector("#task-list");
+taskListEl.addEventListener("click",function(event){
+    // console.log(event.target)
+    let targetElement=event.target;
+    if(targetElement.classList.contains("fa-times-circle")){
+        let actualEl=targetElement.parentElement;
+        let selectedTask=actualEl.innerText;    
+        
+        //To get from Local Storage
+        let taskList = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")):[];
+        taskList=taskList.filter(function(task){
+        return task.trim() !== selectedTask.trim();
+            });
+    
+    localStorage.setItem("tasks",JSON.stringify(taskList));
+    displayTask();
+    }
+});
